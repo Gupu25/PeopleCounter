2b5031e38d31887cd1f5d4f4d8234448e5368a9f
Federico Mejia <federico.9006@gmail.com>
2020-03-04 10:33:39 -0600

    Updating code to Python3, OpenCV4

--- PeopleCounter.py
+++ PeopleCounter.py
@@ -3,22 +3,22 @@
 ##from picamera.array import PiRGBArray
 ##from picamera import PiCamera
 import numpy as np
-import cv2
+import cv2 as cv
 import Person
 import time
 
 try:
     log = open('log.txt',"w")
 except:
-    print "No se puede abrir el archivo log"
+    print( "No se puede abrir el archivo log")
 
 #Contadores de entrada y salida
 cnt_up   = 0
 cnt_down = 0
 
 #Fuente de video
-#cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('peopleCounter.avi')
+#cap = cv.VideoCapture(0)
+cap = cv.VideoCapture('TestVideo.avi')
 #camera = PiCamera()
 ##camera.resolution = (160,120)
 ##camera.framerate = 5
@@ -31,13 +31,13 @@ cap = cv2.VideoCapture('peopleCounter.avi')
 
 #Imprime las propiedades de captura a consola
 for i in range(19):
-    print i, cap.get(i)
+    print( i, cap.get(i))
 
 h = 480
 w = 640
 frameArea = h*w
 areaTH = frameArea/250
-print 'Area Threshold', areaTH
+print( 'Area Threshold', areaTH)
 
 #Lineas de entrada/salida
 line_up = int(2*(h/5))
@@ -46,8 +46,8 @@ line_down   = int(3*(h/5))
 up_limit =   int(1*(h/5))
 down_limit = int(4*(h/5))
 
-print "Red line y:",str(line_down)
-print "Blue line y:", str(line_up)
+print( "Red line y:",str(line_down))
+print( "Blue line y:", str(line_up))
 line_down_color = (255,0,0)
 line_up_color = (0,0,255)
 pt1 =  [0, line_down];
@@ -69,7 +69,7 @@ pts_L4 = np.array([pt7,pt8], np.int32)
 pts_L4 = pts_L4.reshape((-1,1,2))
 
 #Substractor de fondo
-fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
+fgbg = cv.createBackgroundSubtractorMOG2(detectShadows = True)
 
 #Elementos estructurantes para filtros morfoogicos
 kernelOp = np.ones((3,3),np.uint8)
@@ -77,7 +77,7 @@ kernelOp2 = np.ones((5,5),np.uint8)
 kernelCl = np.ones((11,11),np.uint8)
 
 #Variables
-font = cv2.FONT_HERSHEY_SIMPLEX
+font = cv.FONT_HERSHEY_SIMPLEX
 persons = []
 max_p_age = 5
 pid = 1
@@ -100,27 +100,27 @@ while(cap.isOpened()):
 
     #Binariazcion para eliminar sombras (color gris)
     try:
-        ret,imBin= cv2.threshold(fgmask,200,255,cv2.THRESH_BINARY)
-        ret,imBin2 = cv2.threshold(fgmask2,200,255,cv2.THRESH_BINARY)
+        ret,imBin= cv.threshold(fgmask,200,255,cv.THRESH_BINARY)
+        ret,imBin2 = cv.threshold(fgmask2,200,255,cv.THRESH_BINARY)
         #Opening (erode->dilate) para quitar ruido.
-        mask = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, kernelOp)
-        mask2 = cv2.morphologyEx(imBin2, cv2.MORPH_OPEN, kernelOp)
+        mask = cv.morphologyEx(imBin, cv.MORPH_OPEN, kernelOp)
+        mask2 = cv.morphologyEx(imBin2, cv.MORPH_OPEN, kernelOp)
         #Closing (dilate -> erode) para juntar regiones blancas.
-        mask =  cv2.morphologyEx(mask , cv2.MORPH_CLOSE, kernelCl)
-        mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernelCl)
+        mask =  cv.morphologyEx(mask , cv.MORPH_CLOSE, kernelCl)
+        mask2 = cv.morphologyEx(mask2, cv.MORPH_CLOSE, kernelCl)
     except:
         print('EOF')
-        print 'UP:',cnt_up
-        print 'DOWN:',cnt_down
+        print( 'UP:',cnt_up)
+        print ('DOWN:',cnt_down)
         break
     #################
     #   CONTORNOS   #
     #################
     
     # RETR_EXTERNAL returns only extreme outer flags. All child contours are left behind.
-    _, contours0, hierarchy = cv2.findContours(mask2,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours0, hierarchy = cv.findContours(mask2,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
     for cnt in contours0:
-        area = cv2.contourArea(cnt)
+        area = cv.contourArea(cnt)
         if area > areaTH:
             #################
             #   TRACKING    #
@@ -128,10 +128,10 @@ while(cap.isOpened()):
             
             #Falta agregar condiciones para multipersonas, salidas y entradas de pantalla.
             
-            M = cv2.moments(cnt)
+            M = cv.moments(cnt)
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
-            x,y,w,h = cv2.boundingRect(cnt)
+            x,y,w,h = cv.boundingRect(cnt)
 
             new = True
             if cy in range(up_limit,down_limit):
@@ -142,11 +142,11 @@ while(cap.isOpened()):
                         i.updateCoords(cx,cy)   #actualiza coordenadas en el objeto and resets age
                         if i.going_UP(line_down,line_up) == True:
                             cnt_up += 1;
-                            print "ID:",i.getId(),'crossed going up at',time.strftime("%c")
+                            print( "ID:",i.getId(),'crossed going up at',time.strftime("%c"))
                             log.write("ID: "+str(i.getId())+' crossed going up at ' + time.strftime("%c") + '\n')
                         elif i.going_DOWN(line_down,line_up) == True:
                             cnt_down += 1;
-                            print "ID:",i.getId(),'crossed going down at',time.strftime("%c")
+                            print( "ID:",i.getId(),'crossed going down at',time.strftime("%c"))
                             log.write("ID: " + str(i.getId()) + ' crossed going down at ' + time.strftime("%c") + '\n')
                         break
                     if i.getState() == '1':
@@ -166,9 +166,9 @@ while(cap.isOpened()):
             #################
             #   DIBUJOS     #
             #################
-            cv2.circle(frame,(cx,cy), 5, (0,0,255), -1)
-            img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)            
-            #cv2.drawContours(frame, cnt, -1, (0,255,0), 3)
+            cv.circle(frame,(cx,cy), 5, (0,0,255), -1)
+            img = cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)            
+            #cv.drawContours(frame, cnt, -1, (0,255,0), 3)
             
     #END for cnt in contours0
             
@@ -179,32 +179,32 @@ while(cap.isOpened()):
 ##        if len(i.getTracks()) >= 2:
 ##            pts = np.array(i.getTracks(), np.int32)
 ##            pts = pts.reshape((-1,1,2))
-##            frame = cv2.polylines(frame,[pts],False,i.getRGB())
+##            frame = cv.polylines(frame,[pts],False,i.getRGB())
 ##        if i.getId() == 9:
 ##            print str(i.getX()), ',', str(i.getY())
-        cv2.putText(frame, str(i.getId()),(i.getX(),i.getY()),font,0.3,i.getRGB(),1,cv2.LINE_AA)
+        cv.putText(frame, str(i.getId()),(i.getX(),i.getY()),font,0.3,i.getRGB(),1,cv.LINE_AA)
         
     #################
     #   IMAGANES    #
     #################
     str_up = 'UP: '+ str(cnt_up)
     str_down = 'DOWN: '+ str(cnt_down)
-    frame = cv2.polylines(frame,[pts_L1],False,line_down_color,thickness=2)
-    frame = cv2.polylines(frame,[pts_L2],False,line_up_color,thickness=2)
-    frame = cv2.polylines(frame,[pts_L3],False,(255,255,255),thickness=1)
-    frame = cv2.polylines(frame,[pts_L4],False,(255,255,255),thickness=1)
-    cv2.putText(frame, str_up ,(10,40),font,0.5,(255,255,255),2,cv2.LINE_AA)
-    cv2.putText(frame, str_up ,(10,40),font,0.5,(0,0,255),1,cv2.LINE_AA)
-    cv2.putText(frame, str_down ,(10,90),font,0.5,(255,255,255),2,cv2.LINE_AA)
-    cv2.putText(frame, str_down ,(10,90),font,0.5,(255,0,0),1,cv2.LINE_AA)
+    frame = cv.polylines(frame,[pts_L1],False,line_down_color,thickness=2)
+    frame = cv.polylines(frame,[pts_L2],False,line_up_color,thickness=2)
+    frame = cv.polylines(frame,[pts_L3],False,(255,255,255),thickness=1)
+    frame = cv.polylines(frame,[pts_L4],False,(255,255,255),thickness=1)
+    cv.putText(frame, str_up ,(10,40),font,0.5,(255,255,255),2,cv.LINE_AA)
+    cv.putText(frame, str_up ,(10,40),font,0.5,(0,0,255),1,cv.LINE_AA)
+    cv.putText(frame, str_down ,(10,90),font,0.5,(255,255,255),2,cv.LINE_AA)
+    cv.putText(frame, str_down ,(10,90),font,0.5,(255,0,0),1,cv.LINE_AA)
 
-    cv2.imshow('Frame',frame)
-    cv2.imshow('Mask',mask)    
+    cv.imshow('Frame',frame)
+    cv.imshow('Mask',mask)    
     
 
 ##    rawCapture.truncate(0)
     #preisonar ESC para salir
-    k = cv2.waitKey(30) & 0xff
+    k = cv.waitKey(30) & 0xff
     if k == 27:
         break
 #END while(cap.isOpened())
@@ -215,4 +215,4 @@ while(cap.isOpened()):
 log.flush()
 log.close()
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
